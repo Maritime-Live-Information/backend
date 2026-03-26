9a77cea9b8e7799409a0d25df2201ad3d7ae918a
Tarek <103042190+tarek0201@users.noreply.github.com>
2026-03-26 18:59:30 +0100

    Remove userId from CreateUserDeviceRequest and clean up commented code

--- src/main/kotlin/hs/flensburg/marlin/business/api/userLocations/entity/CreateOrUpdateUserLocationRequest.kt
+++ src/main/kotlin/hs/flensburg/marlin/business/api/userLocations/entity/CreateOrUpdateUserLocationRequest.kt
@@ -4,7 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class CreateOrUpdateUserLocationRequest(
-    var userId: Long,
     var locationId: Long,
     var sentHarborNotifications: Boolean
 )
